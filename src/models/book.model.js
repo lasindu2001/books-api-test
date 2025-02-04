a11a1f2383b6cc7474a24cf7f8e4765b1ba5af4d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2025-02-05 00:36:24 +0530

    feat: create in-memory data model for books

---
+++ src/models/book.model.js
@@ -0,0 +1,6 @@
+let books = [
+    { id: 1, name: "Book One", author: "Author One", publishedYear: 2000 },
+    { id: 2, name: "Book Two", author: "Author Two", publishedYear: 2005 },
+];
+
+module.exports = books;
