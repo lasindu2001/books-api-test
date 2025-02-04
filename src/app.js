2c93fce172b9ade703cfa66d25fc3ffabd924047
Lasindu Maduranga <lasindumvp2001@gmail.com>
2025-02-05 01:00:57 +0530

    fix: add centralized error handling middleware

--- src/app.js
+++ src/app.js
@@ -12,7 +12,12 @@ app.use(express.json());
 app.use("/api/books", booksRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Books API!");
+    res.send("Welcome to the Books API!");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Internal Server Error" });
 });
 
 module.exports = app;
