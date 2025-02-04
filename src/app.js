a86aa7472fc5e59f1513e31b5dd920bb666d9a74
Lasindu Maduranga <lasindumvp2001@gmail.com>
2025-02-05 01:04:52 +0530

    docs: integrate Swagger for API documentation

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const booksRoutes = require("./routes/book.routes");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
@@ -19,5 +21,16 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: "Internal Server Error" });
 });
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: { title: "Books API", version: "1.0.0" },
+    },
+    apis: ["./src/routes/*.js"],
+};
+
+const specs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 module.exports = app;
